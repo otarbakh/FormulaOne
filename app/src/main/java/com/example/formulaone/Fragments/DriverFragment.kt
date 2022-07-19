e87bd0bc821875f8eb50e70b96ddb9f0dfc4dcc1
otarbakh <otarbakh@gmail.com>
2022-07-20 02:51:56 +0400

    add delete for Drivers

--- app/src/main/java/com/example/formulaone/Fragments/DriverFragment.kt
+++ app/src/main/java/com/example/formulaone/Fragments/DriverFragment.kt
@@ -52,7 +52,8 @@ class DriverFragment : Fragment() {
                             etAge.text.toString().toInt()
                         )
                     )
-                    notifyItemInserted(1)
+                    notifyDataSetChanged()
+
                 }
             }
         }
